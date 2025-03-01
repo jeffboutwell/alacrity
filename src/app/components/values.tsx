7610a285dfaa23dcbbf349161ec7daa076647e0a
Jeff Boutwell <jeffboutwell@gmail.com>
2025-03-01 12:57:00 -0700

    new layout for values

--- src/app/components/values.tsx
+++ src/app/components/values.tsx
@@ -1,24 +1,14 @@
 import Image from "next/image";
 import React from "react";
-import flexibilityIcon from "../../../public/images/flexibility.png";
-import growthIcon from "../../../public/images/growth.png";
-import missionIcon from "../../../public/images/mission.png";
-import purposeIcon from "../../../public/images/purpose.png";
-import supportIcon from "../../../public/images/support.png";
+import soldierFlag from "../../../public/images/soldier-flag.jpg";
 
 const Values = () => {
   return (
     <div className="Values Content--outer">
       <div className="Content--inner">
-        <h2 className="mb-16">Our Values</h2>
         <div className="Values__itemList">
+          <h2>Our Values</h2>
           <div className="Values__item">
-            <Image
-              src={purposeIcon}
-              width={purposeIcon.width}
-              height={purposeIcon.height}
-              alt="purpose icon"
-            />
             <h3>Purpose</h3>
             <p>
               Working with Alacrity means being part of a mission bigger than
@@ -30,12 +20,6 @@ const Values = () => {
             </p>
           </div>
           <div className="Values__item">
-            <Image
-              src={flexibilityIcon}
-              width={flexibilityIcon.width}
-              height={flexibilityIcon.height}
-              alt="flexibility icon"
-            />
             <h3>Flexibility</h3>
             <p>
               Alacrity offers flexible scheduling to accommodate your lifestyle,
@@ -45,12 +29,6 @@ const Values = () => {
             </p>
           </div>
           <div className="Values__item">
-            <Image
-              src={missionIcon}
-              width={missionIcon.width}
-              height={missionIcon.height}
-              alt="mission icon"
-            />
             <h3>Mission</h3>
             <p>
               Alacrity&#8217;s focus on military and veteran care allows you to
@@ -58,13 +36,7 @@ const Values = () => {
               difference in the lives of those who serve.
             </p>
           </div>
-          <div className="Values__item md:col-start-2">
-            <Image
-              src={supportIcon}
-              width={supportIcon.width}
-              height={supportIcon.height}
-              alt="support icon"
-            />
+          <div className="Values__item">
             <h3>Support</h3>
             <p>
               We understand that healthcare and dental professionals work in
@@ -75,12 +47,6 @@ const Values = () => {
             </p>
           </div>
           <div className="Values__item">
-            <Image
-              src={growthIcon}
-              width={growthIcon.width}
-              height={growthIcon.height}
-              alt="growth icon"
-            />
             <h3>Growth</h3>
             <p>
               By joining Alacrity, you&#8217;ll gain access to a range of
@@ -88,7 +54,14 @@ const Values = () => {
               field while supporting a community that appreciates your work.
             </p>
           </div>
-          <div className="Values__item-photo"></div>
+        </div>
+        <div className="Values__photo">
+          <Image
+            src={soldierFlag}
+            alt="soldier flag"
+            width={soldierFlag.width}
+            height={soldierFlag.height}
+          />
         </div>
       </div>
     </div>
