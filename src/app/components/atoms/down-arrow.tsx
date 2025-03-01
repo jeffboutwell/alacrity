b3595b16fb1f9bd734bc0e06fe17c5e7e117a671
Jeff Boutwell <jeffboutwell@gmail.com>
2025-03-01 13:40:23 -0700

    hero spacing

---
+++ src/app/components/atoms/down-arrow.tsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+const DownArrow = () => {
+  return (
+    <div>
+      <FontAwesomeIcon icon={faArrowDown} className="text-xl mt-12" />
+    </div>
+  );
+};
+
+export default DownArrow;
--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,13 +1,14 @@
+import DownArrow from "./atoms/down-arrow";
 import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div className="Hero w-full py-52 text-white text-center">
-      <h1 className="mx-8 w-auto lg:mx-auto lg:max-w-[40%]">
+    <div className="Hero w-full pt-48 pb-8 text-white text-center">
+      <h1 className="mx-8 w-auto lg:mx-auto lg:max-w-xl leading-[3rem]">
         Where your expertise meets a <em>mission that matters</em>
       </h1>
-      <p className="mx-16 w-auto sm:max-w-96 sm:mx-auto lg:max-w-[35%]">
+      <p className="mx-16 text-sm w-auto sm:max-w-96 sm:mx-auto lg:max-w-[35%]">
         Whether you&#8217;re looking for a temporary, permanent, or contract
         role, we have opportunities that match your skills and career goals.
         <br />
@@ -16,6 +17,7 @@ const Hero = () => {
       <Link href={"#form"} className="Button Button--secondary">
         Apply Now
       </Link>
+      <DownArrow />
     </div>
   );
 };
